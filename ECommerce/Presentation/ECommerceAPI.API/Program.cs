4cf3f4e7c7af3701b4a104c73082be313d888f65
serdar turan <turanserdar@outlook.com.tr>
2023-01-27 23:48:58 +0000

    first commit

--- ECommerce/Presentation/ECommerceAPI.API/Program.cs
+++ ECommerce/Presentation/ECommerceAPI.API/Program.cs
@@ -3,7 +3,7 @@ using ECommerceAPI.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddPersistenceServices();
+//builder.Services.AddPersistenceServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
